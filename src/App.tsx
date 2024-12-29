b8c5744edc2c53e739af3b4530c4e6684939d2a5
ROHIT <dhiman1675@gmail.com>
2024-12-29 19:22:46 +0530

    add loader

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,15 @@ function App() {
     queryFn: () => fetchPokemonCards(1),
   });
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) {
+    return (
+      <div className="text-center m-5">
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
+  }
   if (error) return <div>An error has occurred: {error.message}</div>;
 
   const filteredPokemon = (data ?? []).filter((pokemon) =>
