3901816acee50d169cfc1e50168ab9dbcc8ee490
ROHIT <dhiman1675@gmail.com>
2024-12-30 14:54:40 +0530

    change card design

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,5 @@
 import { PokemonCard } from "../../react-query/Api";
+import "./Card.css";
 
 interface Props {
   cards: PokemonCard[];
@@ -7,17 +8,17 @@ const Card = ({ cards }: Props) => {
   return (
     <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-6 row-cols-xl-7 d-flex justify-content-center">
       {cards.map((pokemon) => (
-        <div key={pokemon.id} className="card text-bg-dark m-3">
-          <div className="card-header">{pokemon.name}</div>
+        <div key={pokemon.id} className="card animated-rainbow-div m-3 p-1">
+          {/* <div className="card-header">{pokemon.name}</div> */}
           <img
             src={pokemon.images.small}
             alt={pokemon.name}
             className="card-img-top"
           />
-          <div className="card-body">
-            <h5 className="card-text">{pokemon.types?.join(", ")} Type</h5>
-            {/* <p className="card-text">{pokemon.flavorText}</p> */}
-          </div>
+          {/* <div className="card-body"> */}
+          {/* <h5 className="card-text">{pokemon.types?.join(", ")} Type</h5> */}
+          {/* <p className="card-text">{pokemon.flavorText}</p> */}
+          {/* </div> */}
         </div>
       ))}
     </div>
